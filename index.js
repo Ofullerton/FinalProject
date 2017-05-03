43b605ea6a45344067310c329532caae6c4456be
Osar Fullerton <ofullerton@mail.csuchico.edu>
2017-05-03 15:29:32 -0700

    added things

--- index.js
+++ index.js
@@ -18,6 +18,42 @@ var giraffes = [
   }
 ];
 
+var sun = [
+{
+  description:"hint4",
+  picture:"https://pbs.twimg.com/profile_images/641353910561566720/VSxsyxs7.jpg",
+  source:"https://twitter.com/sun",
+},
+{
+  description:"hint5",
+  picture:"http://static3.businessinsider.com/image/55c920c4371d2215008bea1d-1190-625/heres-what-would-happen-if-the-sun-disappeared-right-now.jpg",
+  source:"http://www.businessinsider.com/what-would-happen-if-the-sun-disappeared-2015-8"
+},
+{
+  description:"hint6",
+  picture:"http://1000awesomethings.com/wp-content/uploads/2012/01/the-sun.jpg",
+  source:"http://1000awesomethings.com/2012/01/19/66-the-sun/",
+}
+];
+
+var wrench = [
+{
+  description:"hint7",
+  picture:"https://public.snapon.com/R_RRD/Objects_lg/images/OES18B.jpg",
+  source:"https://store.snapon.com/WRENCHES-C629061.aspx",
+},
+{
+  description:"hint8",
+  picture:"http://www.homedepot.com/catalog/productImages/1000/3a/3ae7feaf-1d21-4a3e-83ed-450a5a16ae20_1000.jpg",
+  source:"http://www.homedepot.com/p/Crescent-6-in-and-10-in-Adjustable-Wrench-Set-AT2610CVS/203161685",
+},
+{
+  description:"hint9",
+  picture:"https://images.wihatoolsus.com/catalog/product/cache/1/small_image/300x300/9df78eab33525d08d6e5fb8d27136e95/4/0/40001_1.jpg",
+  source:"https://www.wihatools.com/shop-tools/wrenches",
+}
+];
+
 var i = 0;
 var current;
 function displayHint()
@@ -26,10 +62,39 @@ function displayHint()
   do {
  index = Math.floor(Math.random() * giraffes.length);
   }
-  while (index==current); // this is saying saying that the index has to be the
-  //as the current in order to run again
-  current = index;  //separately this is saying when current = index
+  while (index==current);
+
+  current = index;
 console.log(giraffes[current]);
+document.getElementById("hintPlace").src = giraffes.picture;
+}
+
+var i = 0;
+var current;
+function displayHint1()
+{
+  var index;
+  do {
+ index = Math.floor(Math.random() * sun.length);
+  }
+  while (index==current);
+  current = index;
+console.log(sun[current]);
+document.getElementById("hintPlace").src = sun.picture;
+}
+
+var i = 0;
+var current;
+function displayHint2()
+{
+  var index;
+  do {
+ index = Math.floor(Math.random() * wrench.length);
+  }
+  while (index==current);
+  current = index;
+console.log(wrench[current]);
+document.getElementById("hintPlace").src = wrench.picture;
 }
 
 function validatePic()
@@ -42,4 +107,7 @@ function validatePic()
   {
     document.getElementById('reponse').innerHTML = "Wrong!"
   }
+}
+function nextPage(){
+  window.location.href ="page1.html";
 }
